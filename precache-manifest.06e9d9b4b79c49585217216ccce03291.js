8b9e5c5280f3cd541ad4bcf716af62067c39d3b4
PS-Ddevil <sinhapurushottam911@gmail.com>
2020-01-19 14:54:48 +0530

    Updates

---
+++ precache-manifest.06e9d9b4b79c49585217216ccce03291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee6920015e7077d57de68d8c5c84a24",
+    "url": "/sample-react-app/index.html"
+  },
+  {
+    "revision": "86bd924faae4bab21243",
+    "url": "/sample-react-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5ad5b1fb521a4071e743",
+    "url": "/sample-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "86bd924faae4bab21243",
+    "url": "/sample-react-app/static/js/2.4fd1050f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/sample-react-app/static/js/2.4fd1050f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ad5b1fb521a4071e743",
+    "url": "/sample-react-app/static/js/main.e08e1c7f.chunk.js"
+  },
+  {
+    "revision": "df27849b403c21cbedbb",
+    "url": "/sample-react-app/static/js/runtime-main.2b948448.js"
+  }
+]);
